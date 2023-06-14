6bffa6aa90002141e2fae2daa848e68ebc4e8d4d
Guow <tubond@163.com>
2023-06-14 20:42:33 +0800

    111

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: '/qzimp-mall-2-help/',
+  // publicPath: '/qzimp-mall-2-help/',
   plugins: [
     vue(), 
     vueJsx(),
